cb99b8629f622534866793e2413a16a63188bb1e
Davide Francesco Merico <hds619@gmail.com>
2016-12-07 23:12:25 +0100

    Add Search Function

--- server.js
+++ server.js
@@ -4,14 +4,7 @@ const express = require ('express');
 const mongodb = require ('mongodb').MongoClient;
 const fs = require ('fs');
 const marked = require ('marked');
-const co = require('co');
-const GoogleSearch = require('google-search');
-const auth_data = parseJSON (process.env.auth_data) || require ('./auth_data');
-
-let googleSearch = new GoogleSearch ({
-  key: auth_data.apikey,
-  cx: auth_data.cx
-});
+const request = require ('request');
 
 let app = express ();
 
@@ -21,15 +14,59 @@ app.get ('/search/*', (req, res, next) => {
     return;
   }
 
-  co (function* () {
-    let query = req.params[0];
-    let offset = 0;
+  let query = req.params[0].split (' ').join (',');
+  let offset = 0;
 
-    if ( 'offset' in req.query )
-      offset = +req.query.offset;
+  if ( 'offset' in req.query )
+    offset = +req.query.offset;
 
-    res.writeHead (200, { 'Content-Type': 'application/json' });
-    res.end (JSON.stringify ({ query: query, offset: offset }));
+  res.writeHead (200, { 'Content-Type': 'application/json' });
+
+  request ({
+    url: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search/',
+    method: 'GET',
+    qs: { q: query, offset: offset },
+    headers: {
+      'Ocp-Apim-Subscription-Key': process.env.bingkey
+    }
+  }, (err, bing_res, body) => {
+    if ( err ) {
+      res.end (JSON.stringify ({ error: err }));
+      return;
+    }
+
+    let parsed = JSON.parse (body);
+
+    if ( bing_res.statusCode !== 200 ) {
+      res.end (JSON.stringify ({
+        status: bing_res.statusCode,
+        error: parsed.message }));
+      return;
+    }
+
+    mongodb.connect (process.env.mongoauth, (err, db)=> {
+      if ( err ) {
+        res.end (JSON.stringify ({ error: 'Error in mongodb.connect' }));
+        return;
+      }
+
+      let collection = db.collection ('recent_img_search');
+      collection.findOne ({}, { 'sort': { _id: -1 } }, (err, doc) => {
+        let next_id = 1;
+        if ( !err && doc )
+          next_id = doc._id + 1;
+
+        collection.insert ({
+          _id: next_id,
+          date: Math.floor (new Date ().getTime () / 1000),
+          query: query
+        });
+
+        if ( 'value' in parsed )
+          res.end (JSON.stringify (parsed.value));
+        else res.end (JSON.stringify ([]));
+      });
+    });
   });
 });
 
